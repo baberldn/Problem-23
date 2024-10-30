cd7bce262f0d1466ff1d82dd10c469ea92b72c48
baberldn <berilbadan1903@gmail.com>
2024-10-30 11:17:20 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 
 export default function Form() {
   const emailsInputRef = useRef(null);
 
-  
+ 
+  useEffect(() => {
+    emailsInputRef.current?.focus();
+  }, []); 
+
   const handleFocusClick = () => {
     emailsInputRef.current?.focus();
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
   };
 
   return (
@@ -14,7 +22,7 @@ export default function Form() {
         <h3 className='text-base font-semibold leading-6 text-gray-900'>
           Input&apos;a focus
         </h3>
-        <form className='mt-5 sm:flex sm:items-center'>
+        <form className='mt-5 sm:flex sm:items-center' onSubmit={handleSubmit}>
           <div className='w-full sm:max-w-xs'>
             <label htmlFor='email' className='sr-only'>
               Email
@@ -23,14 +31,14 @@ export default function Form() {
               type='email'
               name='email'
               id='email'
-              ref={emailsInputRef} 
+              ref={emailsInputRef}
               className='px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
               placeholder='you@example.com'
             />
           </div>
           <button
             type='button'
-            onClick={handleFocusClick} 
+            onClick={handleFocusClick}
             className='mt-3 inline-flex w-full items-center justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:ml-3 sm:mt-0 sm:w-auto'
           >
             Focus
